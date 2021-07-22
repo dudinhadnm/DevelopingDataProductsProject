dd17f41eaf3e18bef04a54f62bcc1bed9657232b
dudinhadnm <dudinha.dnm@gmail.com>
2021-07-22 15:03:44 -0300

    first commit

---
+++ DDPcourseProject/server.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(ggplot2)
+shinyServer(function(input, output) {
+    output$out1 <- renderText(input$sliderCarat)
+    model <- lm(price ~ carat, diamonds)
+    model1pred <- reactive({
+        caratInput <- input$sliderCarat
+        predict(model, newdata = data.frame(carat = caratInput))
+    })
+    output$plot1 <- renderPlot({
+        caratInput <- input$sliderCarat
+        ##plot(diamonds$carat, diamonds$price, xlab = "carat", 
+        ##    ylab = "price", bty = "n", pch = 18,
+        ##    xlim = c(0, 6), ylim = c(300, 20000))
+        ##abline(model, col = "red", lwd = 2)
+        ##points(caratInput, model1pred(), col = "red", pch = 18, cex = 2)
+        ##p
+        g <- ggplot(diamonds, aes(x= carat, y = price)) + geom_point(alpha=0.1, colour = "#00928B", shape =21) + geom_smooth(method=lm, se = FALSE, colour = "#A0097D")
+        g <- g + xlim(0, 6) + ylim(300, 20000)
+        dff <- data.frame(carat = caratInput,price = model1pred())
+        g <- g + geom_point(data = dff, fill = "#FFBA49", colour="#A0097D", shape = 21, size = 2, stroke = 1) 
+        g
+    })
+    output$pred1 <- renderText({
+        model1pred()
+    })
+    
+})
---
+++ DDPcourseProject/ui.R
@@ -0,0 +1,65 @@
+library(shiny)
+shinyUI(fluidPage(
+    tags$head(
+        # Note the wrapping of the string in HTML()
+        tags$style(HTML("
+      body {
+        background-color: #D3D4D9;
+        color: black;
+      }
+      h2 {
+        font-family: 'Yusei Magic', sans-serif;
+        color: #A0097D
+      }
+      h3 {
+        font-family: 'Yusei Magic', sans-serif;
+        color: #A0097D
+      }
+      h4 {
+        font-family: 'Yusei Magic', sans-serif;
+        color: #A0097D
+      }
+      .shiny-input-container {
+        color: #00928B;
+      }"))
+    ),
+    titlePanel("Developing data products Course project"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("sliderCarat", "How many carats in your diamond?", 0, 3, value = 0.4, step = 0.1),
+            submitButton("Submit")
+        ),
+        mainPanel(
+            tabsetPanel(type = "tabs", 
+                        tabPanel("App page", 
+                                 br(), 
+                                 plotOutput("plot1"),
+                                 br(),
+                                 h4("Carat:"),
+                                 textOutput("out1"), 
+                                 br(), 
+                                 h4("Predicted price from Model:"),
+                                 textOutput("pred1")), 
+                        tabPanel("Documentation", 
+                                 br(), 
+                                 h3("Documentation"), 
+                                 h4("How to use this shiny app:"),
+                                 p("Use the slider on the left side bar to 
+                                   input a value for the weight of a diamond 
+                                   (in carat), and click the Submit button to 
+                                   see how much that diamond would cost according 
+                                   to a linear regression calculated used from the 
+                                   'diamonds' dataset"),
+                                 h4("Model"),
+                                 p("It calculates the price of a diamond with weight 
+                                 x in carats, based on the diamonds database. The model 
+                                 used is a simple linear regression:"),
+                                 code("model <- lm(price ~ carat, diamonds)"),
+                                 p("Given the model and the 
+                                   value of carats that was input in the slider 
+                                   on the side bar, the price is predicted as shown:"),
+                                 code("predict(model, newdata = data.frame(carat = caratInput))"))
+            )
+        )
+    )
+))
